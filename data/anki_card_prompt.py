ecb6d907afb1303c578b58165997d1eee77cc062
Abdullah  Rahman <abdullahrahman@abdullahs-air.myfiosgateway.com>
2025-02-21 22:29:24 -0500

    Generating prompt but not very good

---
+++ data/anki_card_prompt.py
@@ -0,0 +1,5 @@
+anki_card_prompt = {
+    'goal': "Summarize and anki flash card from following text",
+    "return_format": "The return format should be in <question>|<answer> where <question> is the question part of the flash card and <answer> is the answer part of the flash card",
+    "warnings": "Be warned to not to include foot not such as © Stephane MaarekNOT FOR DISTRIBUTION © Stephane Maarek www.datacumulus.com,  page number"
+}
--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 from typing import Generator
 import argparse
 import PyPDF2
+from data.anki_card_prompt import anki_card_prompt
+import subprocess
+
+
+def generate_prompt(prompot_dict: dict) -> str:
+    '''
+    helps to build effective prompt
+
+    '''
+    goal = prompot_dict['goal']
+    return_format = prompot_dict['return_format']
+    warnings = prompot_dict['warnings']
+
+
+    return f" \n {goal} \n {return_format} \n {warnings}" 
+
+
+def run_llm_model(prompt: str) -> str:
+    
+    # Command to run with shell features (e.g., command substitution)
+    command = f'ollama run llama3.2 "{prompt}"'
+
+    # Execute the command, capturing the output
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    # Check if the command ran successfully
+    if result.returncode == 0:
+        print("Command output:")
+        return result.stdout
+    else:
+        raise ValueError(result.stderr)
+
 
 
 def parse_pdf(fh) -> Generator:
@@ -21,5 +53,7 @@ def main():
         print(page.extract_text())
     
 if __name__ == "__main__":
-    main()
+    prompt = generate_prompt(anki_card_prompt)
+    generate = run_llm_model(prompt)
+    print(generate)
 
