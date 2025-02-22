b0d25dd08c4803e60a69646b1ebfc641a8f7d690
Abdullah  Rahman <abdullahrahman@abdullahs-air.myfiosgateway.com>
2025-02-21 22:49:47 -0500

    Need install bigger model

--- data/anki_card_prompt.py
+++ data/anki_card_prompt.py
@@ -1,5 +1,5 @@
 anki_card_prompt = {
-    'goal': "Summarize and anki flash card from following text",
-    "return_format": "The return format should be in <question>|<answer> where <question> is the question part of the flash card and <answer> is the answer part of the flash card",
+    'goal': "Summarize and anki flash card from following text. Create atleast 5 cards",
+    "return_format": "question and answer going to be separted by | symbol. Only include question and answer. don't add any header",
     "warnings": "Be warned to not to include foot not such as © Stephane MaarekNOT FOR DISTRIBUTION © Stephane Maarek www.datacumulus.com,  page number"
 }
